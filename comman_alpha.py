4b17e49003a919102b0acd6a5c379a9bd955ef89
SteephanSteve <40833438+SteephanSteve@users.noreply.github.com>
2018-07-20 20:08:47 +0530

    Add files via upload

---
+++ comman_alpha.py
@@ -0,0 +1,27 @@
+#!usr/bin/python
+
+
+n=int(input())
+a=[]
+b=[]
+c=0
+for i in range(n):
+    s=raw_input()
+    a.append(s)
+for j in range(len(min(a))):
+    for i in range(n):
+        if i<n-1:
+            if a[i][j]==a[i+1][j]:
+               c+=1
+            i-=1
+    if c==n-1:
+         b.append(a[i][j])
+    else:
+         break
+    c=0
+            
+                     
+print ''.join(b)
+
+
+
